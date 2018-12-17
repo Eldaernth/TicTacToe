f37567ed1de66ce675db358992479bd4789148e6
Joó Norbert <joonorbi12@gmail.com>
2018-12-17 16:43:23 +0100

    na

---
+++ TicTacToe1.py
@@ -0,0 +1,183 @@
+import subprocess
+import keyboard
+
+def title():
+    print("""                                     TTTTTTTTTTTTTTTTTTTTTTT  iiii                                                             
+                                     T:::::::::::::::::::::T i::::i                                                           
+                                     T:::::::::::::::::::::T  iiii                                                            
+                                     T:::::TT:::::::TT:::::T                                                                  
+                                     TTTTTT  T:::::T  TTTTTTiiiiiii     cccccccccccccccc         
+                                             T:::::T        i:::::i   cc:::::::::::::::c               
+                                             T:::::T         i::::i  c:::::::::::::::::c             
+                                             T:::::T         i::::i c:::::::cccccc:::::c             
+                                             T:::::T         i::::i c::::::c     ccccccc             
+                                             T:::::T         i::::i c:::::c                           
+                                             T:::::T         i::::i c:::::c                            
+                                             T:::::T         i::::i c::::::c     ccccccc                        
+                                           TT:::::::TT      i::::::ic:::::::cccccc:::::c                     
+                                           T:::::::::T      i::::::i c:::::::::::::::::c             
+                                           T:::::::::T      i::::::i  cc:::::::::::::::c             
+                                           TTTTTTTTTTT      iiiiiiii    cccccccccccccccc\n""")           
+
+
+    print("""            TTTTTTTTTTTTTTTTTTTTTTT
+            T:::::::::::::::::::::T
+            T:::::::::::::::::::::T
+            T:::::TT:::::::TT:::::T
+            TTTTTT  T:::::T  TTTTTTaaaaaaaaaaaaa      cccccccccccccccc
+                    T:::::T        a::::::::::::a   cc:::::::::::::::c
+                    T:::::T        aaaaaaaaa:::::a c:::::::::::::::::c
+                    T:::::T                 a::::ac:::::::cccccc:::::c
+                    T:::::T          aaaaaaa:::::ac::::::c     ccccccc
+                    T:::::T        aa::::::::::::ac:::::c
+                    T:::::T       a::::aaaa::::::ac:::::c
+                    T:::::T      a::::a    a:::::ac::::::c     ccccccc
+                  TT:::::::TT    a::::a    a:::::ac:::::::cccccc:::::c
+                  T:::::::::T    a:::::aaaa::::::a c:::::::::::::::::c
+                  T:::::::::T     a::::::::::aa:::a cc:::::::::::::::c
+                  TTTTTTTTTTT      aaaaaaaaaa  aaaa   cccccccccccccccc\n""")
+
+
+    print("""                                                         TTTTTTTTTTTTTTTTTTTTTTT
+                                                         T:::::::::::::::::::::T
+                                                         T:::::::::::::::::::::T
+                                                         T:::::TT:::::::TT:::::T
+                                                         TTTTTT  T:::::T  TTTTTTooooooooooo       eeeeeeeeeeee
+                                                                 T:::::T      oo:::::::::::oo   ee::::::::::::ee
+                                                                 T:::::T     o::::o     o::::oe:::::::::::::::::e
+                                                                 T:::::T     o::::o     o::::oe::::::eeeeeeeeeee
+                                                                 T:::::T     o::::o     o::::oe:::::::e
+                                                               TT:::::::TT   o:::::ooooo:::::oe::::::::e
+                                                               T:::::::::T   o:::::::::::::::o e::::::::eeeeeeee
+                                                               T:::::::::T    oo:::::::::::oo   ee:::::::::::::e
+                                                               TTTTTTTTTTT      ooooooooooo       eeeeeeeeeeeeee\n""")
+
+
+def game():
+    subprocess.call("clear",shell=True)
+    title()
+    print(""" 1.One player\n 2.Two player\n 3.back\n""")
+    selection=int(input("Enter choice:"))
+    if selection==2:
+        subprocess.call("clear",shell=True)
+        main()
+    elif selection==3:
+        mainMenu()
+    else:
+        print("Invalid choice. Press enter 1-3!")
+
+def credit():
+    subprocess.call("clear",shell=True)
+    print(""" Created by:                 dBBBBBb  dBBBP     dBBBBb  dBBBBP    dBBBBb  dBBBP 
+                             dB'               dBP dBP.BP                   
+                             dBBBP' dBBP      dBP dBP dBP.BP    dBBBB   dBBP    
+                             dBP    dBP       dBP dBP dBP.BP    dB' BB  dBP      
+                             dBP    dBBBBP    dBP dBP dBBBBP    dBBBBBB dBBBBP\n Peti\n Norbi\n Geri\n""")
+    print("1.back")
+    selection=int(input("Enter choice:"))                            
+    if selection==1:
+        mainMenu()
+    else:
+        print("Invalid choice. Press enter 1!")
+    
+def win1_score():
+    win1=0
+    subprocess.call("clear",shell=True)
+    win1+=1
+    return win1
+
+def tie_score():
+    tie=0
+    subprocess.call("clear",shell=True)
+    tie+=1
+    return tie
+
+def win2_score():
+    win2=0
+    subprocess.call("clear",shell=True)
+    win2+=1
+    return win2    
+def exitgame():
+    subprocess.call("clear",shell=True)
+    print("Good bye!")
+    exit()
+
+def mainMenu():
+    subprocess.call("clear",shell=True)
+    title()
+    print(""" 1.Start\n 2.Credit\n 3.Quit\n""")
+    selection=int(input("Enter choice:"))
+    if selection==1:
+        game()
+    elif selection==2:
+        credit()
+    elif selection==3:
+        exitgame()
+    else:
+        print("Invalid choice. Press enter 1-3!")
+
+def board(map):
+    print("-----")
+    print(map[1]+"|"+map[2]+"|"+map[3])
+    print("-----")
+    print(map[4]+"|"+map[5]+"|"+map[6])
+    print("-----")
+    print(map[7]+"|"+map[8]+"|"+map[9])
+    print("-----")
+
+def winning_condition(map,state, counter):
+    if map[1] == "x" and map[2] == "x" and map[3] == "x" or map[4] == "x" and map[5] == "x" and map[6] == "x" or map[7] == "x" and map[8] == "x" and map[9] == "x" or map[1] == "x" and map[4] == "x" and map[7] == "x" or map[2] == "x" and map[5]  == "x" and map[8] == "x" or map[3] == "x" and map[6] == "x" and map[9] == "x" or map[1] == "x" and map[5] == "x" and map[9] == "x" or map[7] == "x" and map[5] == "x" and map[3] == "x":
+        print ("Player 1 won:  "+str(win1_score()))
+        state == False
+    if map[1] == "o" and map[2] == "o" and map[3] == "o" or map[4] == "o" and map[5] == "o" and map[6] == "o" or map[7] == "o" and map[8] == "o" and map[9] == "o" or map[1] == "o" and map[4] == "o" and map[7] == "o" or map[2] == "o" and map[5]  == "o" and map[8] == "o" or map[3] == "o" and map[6] == "o" and map[9] == "o" or map[1] == "o" and map[5] == "o" and map[9] == "o" or map[7] == "o" and map[5] == "o" and map[3] == "o":
+        print ("Player 2 won:  ")+str(win2_score())
+        state == False
+    if counter == 9:
+        print("Draw:"+str(tie_score()))
+        state == False
+
+def choice(map, counter):
+    try:
+        cho = int(input("Enter a number: "))
+    except ValueError:
+        cho = input('press q to return to main menu')
+        if cho == 'q':
+            mainMenu()
+        else:
+            print('Invalid input')
+            board(map)
+    else:
+        if counter % 2 == 1:
+            if map[cho] != "x":
+                map[cho] = "o" 
+            else:
+                choice(map,counter)
+        else:
+            if map[cho] != "o":
+                map[cho] = "x"
+            else:
+                choice(map,counter)          
+    subprocess.call("clear",shell=True)
+    board(map)      
+
+def main():   
+    map = ["0","1","2","3","4","5","6","7","8","9"]
+    i = 0
+    run = True
+    counter=0
+    while run == True:
+        board(map)
+        choice(map, counter)
+        subprocess.call("clear",shell=True)
+        winning_condition(map,run,counter)
+        i += 1
+        counter +=1
+    if counter == 8:
+        print('draw1')
+        restart = input("Press r to restart") 
+        if restart == "r":
+            main()
+        else:
+            print("Invalid input")
+mainMenu()
+main()
